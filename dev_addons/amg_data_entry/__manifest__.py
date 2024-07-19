bed864bf9e2dcea22816c3da3dea95f6060192f3
ShawnZhou <shawn@advancemarble.com>
2024-07-19 08:42:51 -0400

    First Commit Test for Web Server

--- dev_addons/amg_data_entry/__manifest__.py
+++ dev_addons/amg_data_entry/__manifest__.py
@@ -1,5 +1,7 @@
 {
     'name': 'Data Entry',
-    'author': 'Shawn - AMG'
+    'author': 'Shawn - AMG',
+    'license': 'LGPL-3',
+    'version' : '17.0.1.0',
 
 }
--- dev_addons/amg_data_entry/models/job_details.py
+++ dev_addons/amg_data_entry/models/job_details.py
@@ -2,3 +2,5 @@ from odoo import api, fields, models
 
 class JobDetail(models.Model):
     _name = "amg.jobdetail"
+    _description = "Data Entry/Job Details"
+
